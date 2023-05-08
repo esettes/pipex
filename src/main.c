95fbbd88109176c2575c679992204eb9ce07667b
esettes <ioanaroxana.st@gmail.com>
2023-05-08 21:05:30 +0200

    playing with fork()

--- src/main.c
+++ src/main.c
@@ -9,17 +9,28 @@ int main(int argc, char *argv[])
 {
 	char inbuf[MSGSIZE];
 	int p[2], i;
+	int pid;
+
+	pid = fork();
 
 	if (pipe(p) < 0)
 		exit(1);
   
 	/* continued */
 	/* write pipe */
-  
+	
 	write(p[1], argv[1], MSGSIZE);
 	write(p[1], argv[2], MSGSIZE);
 	write(p[1], argv[3], MSGSIZE);
-  
+
+	if (pid == 0)
+	{
+		printf("Child pid: %i\n", pid);
+	}
+	else
+	{
+		printf("Parent pid: %i\n", pid);
+	}
 	for (i = 0; i < 3; i++) {
 		/* read pipe */
 		read(p[0], inbuf, MSGSIZE);
