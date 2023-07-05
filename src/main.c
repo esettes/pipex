023adafbd39d39c709a076dd3e7509a3349c6c8e
esettes <ioanaroxana.st@gmail.com>
2023-07-05 23:37:53 +0100

    update

--- src/main.c
+++ src/main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 #define MSGSIZE 16
-// char* msg1 = "hello, world #1";
-// char* msg2 = "hello, world #2";
-// char* msg3 = "hello, world #3";
+char* msg1 = "hello, world #1";
+char* msg2 = "hello, world #2";
+char* msg3 = "hello, world #3";
   
 int main(int argc, char *argv[])
 {
@@ -15,13 +16,13 @@ int main(int argc, char *argv[])
 
 	if (pipe(p) < 0)
 		exit(1);
-  
+ 
 	/* continued */
 	/* write pipe */
 	
-	write(p[1], argv[1], MSGSIZE);
-	write(p[1], argv[2], MSGSIZE);
-	write(p[1], argv[3], MSGSIZE);
+	write(p[1], msg1, MSGSIZE);
+	write(p[1], msg2, MSGSIZE);
+	write(p[1], msg3, MSGSIZE);
 
 	if (pid == 0)
 	{
@@ -34,7 +35,9 @@ int main(int argc, char *argv[])
 	for (i = 0; i < 3; i++) {
 		/* read pipe */
 		read(p[0], inbuf, MSGSIZE);
-		printf("% s\n", inbuf);
+		printf("%s\n", inbuf);
 	}
+
+	dup2(p[0], 0);
 	return 0;
 }
